76ce2c67b8dd00a4ad91dc543d65ff256a0451e2
euijunlee <euijunlee@naver.com>
2025-06-27 15:07:55 +0900

    구조 변경:3?

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,4 +1,11 @@
-import ClientProfilePage from './ClientProfile';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+// ✅ ClientProfilePage를 dynamic import로 감쌈
+const ClientProfilePage = dynamic(() => import('./ClientProfile'), {
+  ssr: false, // 서버 사이드 렌더링 비활성화
+});
 
 export default function Page() {
   return <ClientProfilePage />;
